098878ebdad3e2a514bcbd16a62e150ee2b29a4c
Vitto95 <grimaldi.vitto@gmail.com>
2021-05-05 16:30:05 +0200

    frecce sx/dx impostate

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    photos: [
+      "assets/img/books.jpg",
+      "assets/img/bridge-sun.jpg",
+      "assets/img/bridge-moon.jpg",
+      "assets/img/hiking.jpg",
+    ],
+    counterPhotos: 0,
+  },
+  methods: {
+    prevPhoto() {
+      this.counterPhotos--;
+      if (this.counterPhotos < 0) {
+        this.counterPhotos = this.photos.length - 1;
+      }
+    },
+    nextPhoto() {
+      this.counterPhotos++;
+      if (this.counterPhotos === this.photos.length) {
+        this.counterPhotos = 0;
+      }
+      return counterPhotos;
+    },
+  },
+});
